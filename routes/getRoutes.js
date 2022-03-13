04725aa1e79643becabfd95bd1f6d215d54ca2bc
Jeffrey Carr <jeffrey.carr98@gmail.com>
2022-03-13 00:39:30 -0500

    Updated get routes to align with new SQL database format

--- routes/getRoutes.js
+++ routes/getRoutes.js
@@ -1,7 +1,7 @@
 module.exports = (app, pool) => {
     // Perform general GET query
-    function performQuery(query, res) {
-        pool.query(query, (err, result) => {
+    function performQuery(query, queryValues, res) {
+        pool.query(query, queryValues, (err, result) => {
             if(err) {
                 console.log(err);
             }
@@ -12,38 +12,34 @@ module.exports = (app, pool) => {
 
     // Route to get all recipes
     app.get("/api/get", (req,res) => {
-        let q = performQuery("SELECT * FROM recipes ORDER BY name", res);
+        let q = performQuery("SELECT * FROM recipes ORDER BY name", [], res);
     });
 
     // Route to get one recipe
     app.get("/api/get/:id", (req,res) => {
         const id = req.params.id;
-        const query = "SELECT * FROM recipes WHERE id=" + id;
-        let q = performQuery(query, res);
+        let q = performQuery("SELECT * FROM recipes WHERE id=?", [id], res);
     });
 
     // Get all categories
     app.get("/api/getCategories", (req, res) => {
-        let q = performQuery("SELECT * FROM categories ORDER BY name", res);
+        let q = performQuery("SELECT * FROM categories ORDER BY name", [], res);
     })
 
     // Route to get directions for a recipe
     app.get("/api/getDirections/:id", (req, res) => {
         const id = req.params.id;
-        let query = "SELECT * FROM directions WHERE id=" + id + " ORDER BY step_num";
-        let q = performQuery(query, res);
+        let q = performQuery("SELECT * FROM directions WHERE recipe=? ORDER BY step_num", [id], res);
     });
 
     // Route to get all ingredients
     app.get("/api/getIngredients", (req, res) => {
-        const query = ("SELECT name FROM ingredients ORDER BY name");
-        let q = performQuery(query, res);
+        let q = performQuery("SELECT name FROM ingredients ORDER BY name", [], res);
     })
 
     // Route to get ingredients for a recipe
     app.get("/api/getIngredients/:id", (req, res) => {
         const id = req.params.id;
-        const query = ("SELECT * FROM ingredients, rIngredients WHERE rIngredients.id=" + id + " AND rIngredients.ingredient_id=ingredients.id_ingredient;");
-        let q = performQuery(query, res);
+        let q = performQuery("SELECT * FROM ingredients, rIngredients WHERE rIngredients.recipe=? AND rIngredients.ingredient=ingredients.id", [id], res);
     });
 }
